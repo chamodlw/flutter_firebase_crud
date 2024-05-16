21653a79ff4ff07971b61e71f9da1701b51e01b4
chamodlw <chamodweerasinghe@gmail.com>
2024-05-16 19:24:12 +0530

    develop2 commit 2

--- lib/pages/employee.dart
+++ lib/pages/employee.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Employee extends StatefulWidget {
@@ -8,89 +10,143 @@ class Employee extends StatefulWidget {
 }
 
 class _EmployeeState extends State<Employee> {
+  TextEditingController namecontroller = new TextEditingController();
+  TextEditingController agecontroller = new TextEditingController();
+  TextEditingController locationcontroller = new TextEditingController();
+  
+  var Fluttertoast;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            Text("Employee",
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 24.0),
-              ),
-            Text("Firebase",
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 24.0),
-            )
-          ],
-        )
-      ),
+          title: Row(
+        children: [
+          Text(
+            "Employee",
+            style: TextStyle(color: Colors.blue, fontSize: 24.0),
+          ),
+          Text(
+            "Firebase",
+            style: TextStyle(color: Colors.red, fontSize: 24.0),
+          )
+        ],
+      )),
       body: Container(
         margin: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Employee Name", style: TextStyle(color: Colors.black, fontSize:20.0, fontWeight: FontWeight.bold),) ,
+            Text(
+              "Name",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
             SizedBox(
               height: 10.0,
-              ),
+            ),
             Container(
-            padding: EdgeInsets.only(left: 10.0),
+              padding: EdgeInsets.only(left: 10.0),
               decoration: BoxDecoration(
-                border: Border.all(),borderRadius: BorderRadius.circular(10)
-              ),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
               child: TextField(
+                controller: namecontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
             SizedBox(
               height: 15.0,
-              ),
-            Text("Age", style: TextStyle(color: Colors.black, fontSize:20.0, fontWeight: FontWeight.bold),) ,
+            ),
+            Text(
+              "Age",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
             SizedBox(
               height: 10.0,
-              ),
+            ),
             Container(
-            padding: EdgeInsets.only(left: 10.0),
+              padding: EdgeInsets.only(left: 10.0),
               decoration: BoxDecoration(
-                border: Border.all(),borderRadius: BorderRadius.circular(10)
-              ),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
               child: TextField(
+                controller: agecontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
             SizedBox(
               height: 15.0,
-              ),
-            Text("Location", style: TextStyle(color: Colors.black, fontSize:20.0, fontWeight: FontWeight.bold),) ,
+            ),
+            Text(
+              "Location",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
             SizedBox(
               height: 10.0,
-              ),
+            ),
             Container(
-            padding: EdgeInsets.only(left: 10.0),
+              padding: EdgeInsets.only(left: 10.0),
               decoration: BoxDecoration(
-                border: Border.all(),borderRadius: BorderRadius.circular(10)
-              ),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
               child: TextField(
+                controller: locationcontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
             SizedBox(
               height: 20.0,
-              ),
-            Center (child: ElevatedButton(onPressed: (){
-            }, 
-            child: Text("Add",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),
-            )))
-
+            ),
+            Center(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      String Id = randomAlphaNumeric(10);
+                      Map<String, dynamic> employeeInfoMap = {
+                        "Name": namecontroller.text,
+                        "Age": agecontroller.text,
+                        "Id": Id,
+                        "Location": locationcontroller.text
+                      };
+                      await DatabaseMethods()
+                          .addEmployeedetails(employeeInfoMap, Id)
+                          .then((value) {
+                            var Toast;
+                            var ToastGravity;
+                            Fluttertoast.showToast(
+                                  msg: "Employee aded successfully",
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.CENTER,
+                                  timeInSecForIosWeb: 1,
+                                  backgroundColor: Colors.red,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0
+                              );
+                          });
+                    },
+                    child: Text(
+                      "Add",
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                    )))
           ],
-          
-          
         ),
-        
       ),
     );
   }
+}
+
+DatabaseMethods() {}
+
+String randomAlphaNumeric(int i) {
+  // Your existing code here
+
+  return ""; // Add this line to return an empty string
 }
